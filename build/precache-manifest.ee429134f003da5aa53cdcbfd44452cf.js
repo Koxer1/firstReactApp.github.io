943cf3794796e5a9ee5de300110a1e72beaf39ba
Konrad Lomzik <koxer101@gmail.com>
2020-12-28 10:58:39 +0100

    first commit

---
+++ build/precache-manifest.ee429134f003da5aa53cdcbfd44452cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee8778372ff545e1f144",
+    "url": "/static/css/main.a8333635.chunk.css"
+  },
+  {
+    "revision": "ee8778372ff545e1f144",
+    "url": "/static/js/main.ee877837.chunk.js"
+  },
+  {
+    "revision": "46f3678fa23b680027f2",
+    "url": "/static/js/1.46f3678f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d435529e215e225bad59f363836d2179",
+    "url": "/static/media/logo.d435529e.svg"
+  },
+  {
+    "revision": "8b06078bf376d4dfc2308091ffabe01d",
+    "url": "/index.html"
+  }
+];
---
+++ src/views/Root/Root.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./index.css";
+import AppContext from "../../context";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import TwittersView from "../TwittersView/TwittersView";
+import ArticlesView from "../ArticlesView/ArticlesView";
+import NotesView from "../NotesView/NotesView";
+import Header from "../../components/Header/Header";
+import Modal from "../../components/Modal/Modal";
+
+class Root extends React.Component {
+  state = {
+    twitter: [
+      {
+        title: "Konrad Łomzik",
+        description: "Brak opisu",
+        link: "https://google.com",
+        image: "https://picsum.photos/200",
+      },
+      {
+        title: "Kacper Prokop",
+        description: "Brak opisu",
+        link: "https://google.com",
+        image: "https://picsum.photos/300",
+      },
+      {
+        title: "Weronika Zaremba",
+        description: "Brak opisu",
+        link: "https://google.com",
+        image: "https://picsum.photos/301",
+      },
+      {
+        title: "Natalia Zaremba",
+        description: "Brak opisu",
+        link: "https://google.com",
+        image: "https://picsum.photos/302",
+      },
+    ],
+    article: [],
+    note: [],
+    isModalOpen: false,
+  };
+
+  addItem = (e, newItem) => {
+    e.preventDefault();
+
+    this.setState(prevState => ({
+      [newItem.type]: [...prevState[newItem.type], newItem],
+    }));
+
+    this.closeModal();
+  };
+
+  openModal = () => {
+    this.setState({
+      isModalOpen: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false,
+    });
+  };
+
+  render() {
+    const { isModalOpen } = this.state;
+    const contextElements = {
+      ...this.state,
+      addItem: this.addItem,
+    };
+
+    return (
+      <BrowserRouter>
+        <AppContext.Provider value={contextElements}>
+          <Header openModalFn={this.openModal} />
+          <Switch>
+            <Route exact path="/" component={TwittersView} />
+            <Route path="/articles" component={ArticlesView} />
+            <Route path="/notes" component={NotesView} />
+          </Switch>
+          {isModalOpen && <Modal closeModalFn={this.closeModal} />}
+        </AppContext.Provider>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Root;
+
+// 3. Dopasować Form.js do nowych potrzeb
+// 4. Przystosować widoki podstron do nowych itemów
+// 5. Wyświetlać odpowiednie notatki na podstronach
